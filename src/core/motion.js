f3e83aa44303427f383a53f7f9d6c8c0b2ea50de
CharlesD61994 <charles.dugre@live.ca>
2026-04-15 19:56:58 -0400

    Create motion.js

---
+++ src/core/motion.js
@@ -0,0 +1,5 @@
+export const KALEIDO_EASE = "cubic-bezier(0.22, 1, 0.36, 1)";
+export const KALEIDO_TIMING_FAST = "180ms";
+export const KALEIDO_TIMING_BASE = "220ms";
+export const KALEIDO_TIMING_SCREEN = "260ms";
+export const KALEIDO_TIMING_SLOW = "560ms";
